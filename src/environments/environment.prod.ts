90ee29d3db8b36528dfbed2e5ef070c8a5057427
Akshay Sangani <akshay@thetatechnolabs.com>
2020-06-18 19:29:44 +0530

    URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://atunwapodcasts.com:7000',
-  img_uri: 'https://atunwapodcasts.com/api/'
+  base_url: 'https://api.atunwapodcasts.com:7000',
+  img_uri: 'https://api.atunwapodcasts.com/'
 };
