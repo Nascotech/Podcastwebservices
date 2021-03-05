d7f3a3a77837eeb82584adf514c6b9a2fb375d01
Akshay Sangani <akshay@thetatechnolabs.com>
2021-03-05 11:20:02 +0530

    - Publisher view: domain updated

--- src/app/modules/publishers/publishers.component.ts
+++ src/app/modules/publishers/publishers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-publishers',
@@ -9,33 +10,42 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./publishers.component.css']
 })
 export class PublishersComponent implements OnInit {
-  publisherdata:any;
-  photoUrl:any;
-  finalcolor:any;
-  constructor(private toastr: ToastrService,
-    private router:Router) { }
+
+  publisherdata: any;
+  photoUrl: any;
+  finalcolor: any;
+  checkEnv = false;
+  domain = "atunwapodcasts.com";
+
+  constructor(
+    private toastr: ToastrService,
+    private commonService: CommonService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.photoUrl=environment.img_uri;
+    this.photoUrl = environment.img_uri;
     this.publisherdata = JSON.parse(localStorage.getItem('publisherdetail'));
-    this.finalcolor= this.publisherdata.headerColor;
+    this.finalcolor = this.publisherdata.headerColor;
+    this.checkEnv = this.commonService.CHECK_ENV.isDev;
+    console.log(this.checkEnv);
+
+    if(this.checkEnv) {
+      this.domain = "devpub.atunwapodcasts.com";
+    }
   }
 
-  gotobackpage()
-  {
+  gotobackpage() {
     this.router.navigate(['/dashboard'])
   }
 
-  redirectToPage(link:string) {
-    if(!link) return '';
-      return !(link.startsWith("http://") || link.startsWith("https://")) ? "http://" + link : link;    
-    
+  redirectToPage(link: string) {
+    if (!link) return '';
+    return !(link.startsWith("http://") || link.startsWith("https://")) ? "http://" + link : link;
   }
 
-  gotoeditpage(publisher)
-  {
-    this.router.navigate(['/edit/'+publisher.id]);
-    localStorage.setItem('publisherdetail',JSON.stringify(publisher))
+  gotoeditpage(publisher) {
+    this.router.navigate(['/edit/' + publisher.id]);
+    localStorage.setItem('publisherdetail', JSON.stringify(publisher))
   }
-
 }
